9af416c2829de1ca0c7a47a51907690cd43efd7a
meltingshine <meltingshine@gmail.com>
2020-06-04 21:32:23 +0900

    프래그먼트 생성 완료 그러나 화면에는 올리지 않은

--- app/src/main/java/com/example/fragmentpractice_20200604/fragments/FirstFragment.kt
+++ app/src/main/java/com/example/fragmentpractice_20200604/fragments/FirstFragment.kt
@@ -1,7 +1,25 @@
 package com.example.fragmentpractice_20200604.fragments
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.fragmentpractice_20200604.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
-class FirstFragment : Fragment {
+class FirstFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first,container,false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        fragmentMsgTxt.text = "안녕하세요"
+    }
 
 }
