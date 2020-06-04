2e9e97b2f3d77727af56d0bb452c597b1bf4e140
meltingshine <meltingshine@gmail.com>
2020-06-04 22:10:31 +0900

    프래그먼트를 뿌려주는 어댑터 클래스를 생성

---
+++ app/src/main/java/com/example/fragmentpractice_20200604/Adapters/MyPagerAdapter.kt
@@ -0,0 +1,27 @@
+package com.example.fragmentpractice_20200604.Adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.fragmentpractice_20200604.fragments.FirstFragment
+import com.example.fragmentpractice_20200604.fragments.SecondFragment
+import com.example.fragmentpractice_20200604.fragments.ThirdFragment
+
+class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getItem(position: Int): Fragment {
+        if (position == 0) {
+            return FirstFragment()
+        } else if (position == 1) {
+            return SecondFragment()
+        } else {
+            return ThirdFragment()
+
+        }
+    }
+
+    override fun getCount(): Int {
+
+    }
+
+
+}
