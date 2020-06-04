aabfa5cc7295d614251d4f3c329f8b41bcf1356b
meltingshine <meltingshine@gmail.com>
2020-06-04 22:18:38 +0900

    코틀린의 when을 활용해따

--- app/src/main/java/com/example/fragmentpractice_20200604/Adapters/MyPagerAdapter.kt
+++ app/src/main/java/com/example/fragmentpractice_20200604/Adapters/MyPagerAdapter.kt
@@ -9,18 +9,26 @@ import com.example.fragmentpractice_20200604.fragments.ThirdFragment
 
 class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
     override fun getItem(position: Int): Fragment {
-        if (position == 0) {
-            return FirstFragment()
-        } else if (position == 1) {
-            return SecondFragment()
-        } else {
-            return ThirdFragment()
 
+        return when(position) {
+            0 -> FirstFragment()
+            1 -> SecondFragment()
+            else -> ThirdFragment()
         }
+
+//        if (position == 0) {
+//            return FirstFragment()
+//        } else if (position == 1) {
+//            return SecondFragment()
+//        } else {
+//            return ThirdFragment()
+//
+//        }
+
     }
 
     override fun getCount(): Int {
-    return 3 //총 페이지 숫자
+        return 3 //총 페이지 숫자
     }
 
 
